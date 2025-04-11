8f5b220c715264c4ea35dfffe61d5e3f4216a4fa
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-04-11 22:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here
 
+let btnDay = document.getElementById("swap")
+        let main = document.getElementById("app")
+        function swapTheme(){
+            if(main.classList.contains("day")){
+                main.classList.remove("day")
+                main.classList.add("night")
+            }else{
+                main.classList.remove("night")
+                main.classList.add("day")
+            }
+            if(btnDay.classList.contains("button_day")){
+                btnDay.classList.remove("button_day")
+                btnDay.classList.add("button_night")
+            }else{
+                btnDay.classList.remove("button_night")
+                btnDay.classList.add("button_day")
+            }
+        }
+        btnDay.addEventListener("click",swapTheme)
